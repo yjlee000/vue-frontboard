f5ce930705dba3bc6c92d4c710de759c061cb911
Yejin <hil07031@gmail.com>
2023-10-24 01:05:03 +0900

    사진 업로드 버튼 배치

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <input type="file" @change="onFileChange" accept="image/*">
+      <img :src="imageSrc" v-if="imageSrc"/>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    setup() {
+      const imageSrc = ref(null);
+  
+      const onFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+          imageSrc.value = URL.createObjectURL(file);
+        }
+      };
+  
+      return { imageSrc, onFileChange };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  img {
+    width: 300px;
+  }
+  </style>
